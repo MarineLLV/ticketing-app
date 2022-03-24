f833174ddab43f1bbf0b83a3131dfbbc5eedd9b9
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-24 16:32:13 +0100

    added async

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,30 +1,31 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-errors';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-errors'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
     body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be between 4 and 20 characters')
-    ],
-    (req: Request, res: Response) => {
-        const errors = validationResult(req);
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req)
 
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array())
+    }
 
-        console.log('Creating a user...');
-        throw new DatabaseConnectionError();
+    console.log('Creating a user...')
+    throw new DatabaseConnectionError()
 
-        res.send({});
-});
+    res.send({})
+  },
+)
 
-export { router as signupRouter };
+export { router as signupRouter }
