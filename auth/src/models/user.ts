c773c85a5000b8c2b4317cfd46f0d4af9a1640a8
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-25 17:10:04 +0100

    updated user model with hashed password

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { Password } from './../services/password'
 
 // Interface that describes the properties that are required to create a new User
 interface UserAttrs {
@@ -27,6 +28,17 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 })
+
+// Access to the document
+userSchema.pre('save', async function (done) {
+  // Ckeck if the user's password has been modified
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
+})
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
 }
