3be68687838324f507bd932c602ca19bbf8ed542
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-25 16:24:49 +0100

    user creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-errors'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 const router = express.Router()
 
@@ -21,10 +21,20 @@ router.post(
       throw new RequestValidationError(errors.array())
     }
 
-    console.log('Creating a user...')
-    throw new DatabaseConnectionError()
+    const { email, password } = req.body
 
-    res.send({})
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      console.log('Email in use')
+      return res.send({})
+    }
+
+    // Create user and save into DB
+    const user = User.build({ email, password })
+    await user.save()
+
+    res.status(201).send(user)
   },
 )
 
