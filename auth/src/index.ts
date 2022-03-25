cbb4c9938d31d17532036601c0cff2e98b4626d9
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-25 12:55:52 +0100

    connection to MongoDB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -23,6 +24,18 @@ app.get('*', async (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
+// Connect to the MongoDB pod service
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to MongoDb')
+  } catch (err) {
+    console.error(err)
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000')
+  })
+}
+
+start()
