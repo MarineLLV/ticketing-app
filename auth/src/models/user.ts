2a3646b741e8bfef1f459d9d3e5f8eba2f163df6
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-25 13:36:15 +0100

    defined User Document properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,8 +7,14 @@ interface UserAttrs {
 }
 
 // Interface that describes the properties that a User Model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc
+}
+
+// Interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
 }
 
 const userSchema = new mongoose.Schema({
@@ -25,6 +31,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
 }
 
-const User = mongoose.model<any, UserModel>('User', userSchema)
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
 
 export { User }
