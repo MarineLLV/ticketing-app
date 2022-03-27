526e72d45db787d6db06cf6a2a218a11d40ea975
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-27 21:13:50 +0200

    created custom hook for requests

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,23 +1,22 @@
 import { useState } from 'react';
 import axios from 'axios';
+import UseRequest from '../../hooks/use-request';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors } = UseRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        }
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            })
-            console.log(response.data)
-        } catch (err) {
-            // console.log(err.response.data);
-            setErrors(err.response.data.errors);
-        }
+        doRequest();
     };
 
     return (
@@ -40,13 +39,7 @@ const SignUp = () => {
                     className="form-control"
                 />
             </div>
-            {errors.length > 0 && <div className='alert alert-danger'>
-                <h4>Ooops...</h4>
-                <ul className='my-0'>
-                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
-                </ul>
-            </div>}
-
+            {errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
